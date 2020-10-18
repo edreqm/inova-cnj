96e011c5ab4eb8ddb49c51ae87c644cdbbbdc366
Rodrigo M. Possa <rodrigmp@trt3.jus.br>
2020-10-18 12:35:17 -0300

    Versionamento de tabelas de dominio de complementos e alteraçào da transformação para criar e preencher colunas ds_movimento e ds_movimento_complementos

---
+++ src/tb_tipo_complementos_cnj.sql
@@ -0,0 +1,44 @@
+create table tb_tipo_complementos_cnj (
+	cd_tipo_complemento integer,
+	cd_classificacao char(1),
+	ds_tipo_complemento varchar(100)
+)
+
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(1,'nome_da_parte','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(2,'tipo_de_distribuicao_redistribuicao','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(3,'tipo_de_conclusao','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(4,'tipo_de_documento','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(5,'ato_publicado','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(6,'resultado','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(7,'destino','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(8,'numero_do_processo','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(9,'numero_de_volumes_e_apensos','L');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(10,'tipo_da_decisao_anterior','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(11,'situacao','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(12,'data_hora','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(13,'destinatario','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(14,'data','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(15,'situacao_da_audiencia','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(16,'tipo_de_audiencia','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(17,'motivo_da_redistribuicao','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(18,'motivo_da_remessa','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(19,'tipo_de_peticao','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(20,'nome_do_juizo','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(21,'local','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(22,'tribunal','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(23,'tipo_de_deliberacao','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(25,'movimento_retificado','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(26,'classe_anterior','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(27,'classe_nova','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(28,'tipo_tema_controversia','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(29,'numero_tema_controversia_STF','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(30,'numero_tema_controversia_tribunal_superior','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(31,'destinatario_de_medida_protetiva','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(32,'tipo_de_medida_protetiva','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(33,'nome_do_incidente_repetitivo','L');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(34,'motivos_de_registro','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(35,'numero_tema_controversia_tribunal','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(36,'dirigida_por','T');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(37,'Tipo_Movimentacao_Partido','I');
+insert into tb_tipo_complementos_cnj (cd_tipo_complemento, ds_tipo_complemento, cd_classificacao) values(38,'Situacao_Candidato','I');
+
